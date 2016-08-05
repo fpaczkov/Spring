422ef976bb80d5c834e3055a4f29007b3a88302f
fpaczkov <fpaczkov@gmail.com>
2016-08-05 16:14:42 +0200

    Po dodaniu kilku produktów do strony

--- src/main/java/com/packt/webstore/controller/ProductController.java
+++ src/main/java/com/packt/webstore/controller/ProductController.java
@@ -2,24 +2,22 @@ package com.packt.webstore.controller;
 
 import java.math.BigDecimal;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.packt.webstore.domain.Product;
+import com.packt.webstore.domain.repository.ProductRepository;
 
 @Controller
 public class ProductController {
 
+	@Autowired
+	private ProductRepository productRepository;
+	
 	@RequestMapping("/products")
 	public String list(Model model){
-		Product iphone = new Product("P1234","iPhone 5s", new BigDecimal(500));
-		iphone.setDescription("Apple iPhone 5s, smartfon z 4-calowym "
-				+ "wyœwietlaczem o rozdielczoœci 640x1136 oraz 8-megapikselowym aparetem");
-		iphone.setCategory("Smart Phone");
-		iphone.setManufacturer("Apple");
-		iphone.setUnitsInStock(1000);
-		model.addAttribute("product", iphone);
+		model.addAttribute("products", productRepository.getAllProducts());
 		return "products";
 	}
 }
