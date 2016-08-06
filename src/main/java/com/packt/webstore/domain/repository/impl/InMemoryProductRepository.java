b6c7f1c7a7a9273621ebb804bcd61100264c495d
fpaczkov <fpaczkov@gmail.com>
2016-08-06 23:41:53 +0200

    Spring MVC 2/69 Koniec

--- src/main/java/com/packt/webstore/domain/repository/impl/InMemoryProductRepository.java
+++ src/main/java/com/packt/webstore/domain/repository/impl/InMemoryProductRepository.java
@@ -47,4 +47,19 @@ public class InMemoryProductRepository implements ProductRepository {
 		return listOfProducts;
 	}
 	
+
+	public Product getProductById(String productId) {
+		Product productById = null;
+		for(Product product:listOfProducts){
+			if(product!=null && product.getProductId()!=null &&
+					product.getProductId().equals(productId)){
+				productById = product;
+				break;
+			}
+		}
+		if(productById == null){
+			throw new IllegalArgumentException("Brak produktu o wskazanym id:"+productId);
+		}
+		return productById;
+	}
 }
