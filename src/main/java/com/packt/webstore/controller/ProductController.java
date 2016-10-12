e5fcc84afe1707a37c496ba243f2bc71763429ec
fpaczkov <fpaczkov@gmail.com>
2016-10-13 01:39:26 +0200

    Spring MVC 3/84 Krótki test

--- src/main/java/com/packt/webstore/controller/ProductController.java
+++ src/main/java/com/packt/webstore/controller/ProductController.java
@@ -5,19 +5,34 @@ import java.math.BigDecimal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.packt.webstore.domain.repository.ProductRepository;
+import com.packt.webstore.service.ProductService;
 
-@Controller
+@Controller @RequestMapping("/products")
 public class ProductController {
 
 	@Autowired
-	private ProductRepository productRepository;
+	private ProductService productService;
+//	private ProductRepository productRepository;
 	
-	@RequestMapping("/products")
+	@RequestMapping
 	public String list(Model model){
-		model.addAttribute("products", productRepository.getAllProducts());
+		model.addAttribute("products", productService.getAllProducts());
+		return "products";
+	}
+	
+	@RequestMapping("/all")
+	public String allProducts(Model model){
+		model.addAttribute("products", productService.getAllProducts());
+		return "products";
+	}
+	
+	@RequestMapping("/{category}")
+	public String getProductsByCategory(Model model, @PathVariable("category") String productCategory){
+		model.addAttribute("products", productService.getProductsByCategory(productCategory));
 		return "products";
 	}
 }
---
+++ src/main/java/com/packt/webstore/service/impl/ProductServiceImpl.java
@@ -0,0 +1,26 @@
+package com.packt.webstore.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.packt.webstore.domain.Product;
+import com.packt.webstore.domain.repository.ProductRepository;
+import com.packt.webstore.service.ProductService;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+	@Autowired
+	private ProductRepository productRepostiory;
+	
+	public List<Product> getAllProducts(){
+		return productRepostiory.getAllProducts();
+	}
+	public Product getProductById(String productID){
+		return productRepostiory.getProductById(productID);
+	}
+	public List<Product> getProductsByCategory(String category){
+		return productRepostiory.getProductsByCategory(category);
+	}	
+}
